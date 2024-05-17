395905a09bff5480e338570705c7c123a04afdae
Bernard Lee <lwkbite@gmail.com>
2024-05-17 22:34:22 +0800

    Add basic dashboard design.

--- student/views.py
+++ student/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
+from allauth.socialaccount.models import SocialAccount
 
 # Create your views here.
 def root(request):
     return redirect('/student/dashboard')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    user = request.user
+    context = {}
+
+    if user.is_authenticated:
+        social_account = SocialAccount.objects.filter(user=user, provider='microsoft').first()
+        if social_account:
+            extra_data = social_account.extra_data
+            avatar_url = extra_data.get('photo', {}).get('url')
+            context['avatar_url'] = avatar_url
+    
+    return render(request, 'dashboard.html', context)
