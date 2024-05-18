5e9d8edc6fae0efe2adc2e6b59ee4fdd44d8bf93
Bernard Lee <lwkbite@gmail.com>
2024-05-18 14:07:11 +0800

    Load my course dynamically (from database).

--- lms/views.py
+++ lms/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
 from allauth.socialaccount.models import SocialAccount
 
+from lms.models import EnrolledCourse
+
 def login(request):
     return render(request, 'login.html')
 
 def student_dashboard(request):
     user = request.user
+    uid = request.user.id
     context = {}
 
-    if user.is_authenticated:
-        social_account = SocialAccount.objects.filter(user=user, provider='microsoft').first()
-        if social_account:
-            extra_data = social_account.extra_data
-            avatar_url = extra_data.get('photo', {}).get('url')
-            context['avatar_url'] = avatar_url
+    if not user.is_authenticated:
+        return redirect("/");
+
+    # Get avatar
+    social_account = SocialAccount.objects.filter(user=user, provider='microsoft').first()
+    if social_account:
+        extra_data = social_account.extra_data
+        avatar_url = extra_data.get('photo', {}).get('url')
+        context['avatar_url'] = avatar_url
+    
+    # Get enrolled course
+    my_courses = EnrolledCourse.objects.filter(user_id=uid).select_related('course')
+    context['my_courses'] = my_courses;
     
     return render(request, 'dashboard.html', context)
