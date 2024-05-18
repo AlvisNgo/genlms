fcf3272afcc9fdecb1e519cf2e7a546f1b152a64
Bernard Lee <lwkbite@gmail.com>
2024-05-18 13:41:08 +0800

    Create the database structure for course and enrolled courses.

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    course_name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.course_name
+
+# Composite key in Django referenced from https://stackoverflow.com/a/65005218
+class EnrolledCourse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_id', 'course_id'], name='composite_key')
+        ]
