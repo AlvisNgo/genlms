661e0078eefc5bdaacc6282f4ba2653b2254042e
Bernard Lee <lwkbite@gmail.com>
2024-05-20 11:35:17 +0800

    Allow adding LMS admin to course.

--- lms/admin.py
+++ lms/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin, messages
-from lms.models import Admin, Course
+from lms.models import Admin, Course, CourseAdmin
 
 # Empty admin to let django knows we allow adding of admin via the /admin dashboard.
 @admin.register(Admin)
@@ -9,3 +9,15 @@ class AddLMSAdmin(admin.ModelAdmin):
 @admin.register(Course)
 class AddCourse(admin.ModelAdmin):
     pass
+
+@admin.register(CourseAdmin)
+class AddCourseAdmin(admin.ModelAdmin):
+    list_display = ('admin_name', 'course_name')
+
+    def admin_name(self, obj):
+        return obj.admin
+    admin_name.short_description = 'Admin'
+
+    def course_name(self, obj):
+        return obj.course
+    course_name.short_description = 'Course'
--- lms/models.py
+++ lms/models.py
@@ -20,7 +20,21 @@ class EnrolledCourse(models.Model):
         ]
 
 class Admin(models.Model):
-    user = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
+    admin_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{} ({})'.format(self.user.email, self.user.first_name)
+
+
+class CourseAdmin(models.Model):
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['admin_id', 'course_id'], name='composite_key_courseadmin')
+        ]
+    
+    def __str__(self):
+        return '{} ({})'.format(self.admin.user.email, self.admin.user.first_name)
