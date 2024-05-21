33b761c2ada0fe93342be671414baa8c6cf92f9f
AlvisNgo <148420881+AlvisNgo@users.noreply.github.com>
2024-05-21 21:11:01 +0800

    Revert "Update urls.py"
    This reverts commit 0ad566aa11e7b665f548555777c65592fe0e05d4.

--- lms/urls.py
+++ lms/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.login, name = "login"),
     path("student/dashboard", views.student_dashboard, name = "dashboard"),
-    path("student/course/<int:id>", views.student_course_info, name = "course"),
+    path("student/course/<int:id>", views.student_course_info, name = "course")
     path("student/dashboard/profile/", views.profile_view, name="profile"),
 ]
