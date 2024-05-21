e3fe2e4b08ad51718d1db28dd6988c670f6155f3
seannicholas <143307563+seannicholas@users.noreply.github.com>
2024-05-21 21:24:19 +0800

    added discussion board to urls, views, and template.
    added prettier ignore for formatting purposes

--- lms/urls.py
+++ lms/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name = "login"),
-    path("student/dashboard", views.student_dashboard, name = "dashboard"),
-    path("student/course/<int:id>", views.student_course_info, name = "course")
+    path("", views.login, name="login"),
+    path("student/dashboard", views.student_dashboard, name="dashboard"),
+    path("student/course/<int:id>", views.student_course_info, name="course"),
+    path("student/course/discussionboard/<int:id>",
+         views.discussion_board, name="discussionboard"),
     path("student/dashboard/profile/", views.profile_view, name="profile"),
 ]
--- lms/views.py
+++ lms/views.py
@@ -3,8 +3,10 @@ from allauth.socialaccount.models import SocialAccount
 
 from lms.models import EnrolledCourse, Admin, CourseAdmin, Course
 
+
 def login(request):
     return render(request, 'login.html')
+
 
 def student_dashboard(request):
     user = request.user
@@ -12,35 +14,41 @@ def student_dashboard(request):
     context = {}
 
     if not user.is_authenticated:
-        return redirect("/");
+        return redirect("/")
 
     # Get avatar
-    social_account = SocialAccount.objects.filter(user=user, provider='microsoft').first()
+    social_account = SocialAccount.objects.filter(
+        user=user, provider='microsoft').first()
     if social_account:
         extra_data = social_account.extra_data
         avatar_url = extra_data.get('photo', {}).get('url')
         context['avatar_url'] = avatar_url
-    
+
     # Check if user is lms admin
     is_admin = Admin.objects.filter(user_id=uid).exists()
-    
+
     # Get enrolled course if student, else get assigned course (CourseAdmin)
     if (not is_admin):
-        my_courses = EnrolledCourse.objects.filter(user_id=uid).select_related('course')
-        context['my_courses'] = my_courses;
+        my_courses = EnrolledCourse.objects.filter(
+            user_id=uid).select_related('course')
+        context['my_courses'] = my_courses
     else:
         admin_info = Admin.objects.get(user_id=uid)
-        my_courses = CourseAdmin.objects.filter(admin_id=admin_info.admin_id).select_related('course')
-        context['my_courses'] = my_courses;
+        my_courses = CourseAdmin.objects.filter(
+            admin_id=admin_info.admin_id).select_related('course')
+        context['my_courses'] = my_courses
     return render(request, 'dashboard.html', context)
+
 
 def student_course_info(request, id):
     context = {}
     # Get enrolled course corresponding course id, then get course details
-    course_info = Course.objects.filter(pk=EnrolledCourse.objects.filter(pk=id).values_list("course_id",flat=True)[0]).values()
-    context['course_info'] = course_info;
+    course_info = Course.objects.filter(pk=EnrolledCourse.objects.filter(
+        pk=id).values_list("course_id", flat=True)[0]).values()
+    context['course_info'] = course_info
     print(context)
     return render(request, 'course.html', context)
+
 
 def profile_view(request):
     user = request.user
@@ -50,9 +58,18 @@ def profile_view(request):
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            print(f"Redirecting to profile: {profile}") 
+            print(f"Redirecting to profile: {profile}")
             return redirect('profile')
     else:
         form = ProfileForm(instance=profile)
 
     return render(request, 'profile.html', {'form': form, 'profile': profile, 'user': user})
+
+
+def discussion_board(request, id):
+    context = {}
+    # Get enrolled course corresponding course id, then get course details
+    course_info = Course.objects.filter(pk=EnrolledCourse.objects.filter(
+        pk=id).values_list("course_id", flat=True)[0]).values()
+    context['course_info'] = course_info
+    return render(request, 'discussionboard.html', context)
