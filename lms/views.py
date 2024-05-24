8704a59b3b34f6356b709fbc6f0456fe5786ef9a
Bernard Lee <lwkbite@gmail.com>
2024-05-24 13:48:36 +0800

    Fix course view loop, add dynamic announcement view.

--- lms/views.py
+++ lms/views.py
@@ -45,20 +45,15 @@ def student_dashboard(request):
 
 
 def student_course_info(request, id):
-    
     # Get enrolled course corresponding course id, then get course details
-    course_info = Course.objects.filter(pk=id).values()
-
-    try:
-        courseAnnouncement_info = CourseAnnouncement.objects.latest( "created_at", "updated_at")
-    except:
-        courseAnnouncement_info = None
-
+    course_info = get_object_or_404(Course, pk=id)
+    courseAnnouncement_info = CourseAnnouncement.objects.filter(course=course_info).order_by('-created_at').first()
     
     context = {
-        'course_info':course_info,
-        'courseAnnouncement_info':courseAnnouncement_info,
-        }
+        'course_info': course_info,
+        'courseAnnouncement_info': courseAnnouncement_info,
+    }
+
     print(context)
     return render(request, 'course.html', context)
 
