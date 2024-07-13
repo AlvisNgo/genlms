7f77b78295bab1c3af92cb5f28c7bb537db7a132
AlvisNgo <148420881+AlvisNgo@users.noreply.github.com>
2024-07-13 23:42:49 +0800

    fixed group_discard bug

--- lms/views/consumer.py
+++ lms/views/consumer.py
@@ -6,10 +6,11 @@ from lms.models import User,Message,ChatRoom,ChatRoomUser
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         channel_user = self.scope['url_route']['kwargs']['userid']
-        self.groups = await sync_to_async(list)(ChatRoom.objects.filter(chatroomuser__user_id=channel_user))
+        channel_groups = await sync_to_async(list)(ChatRoom.objects.filter(chatroomuser__user_id=channel_user).values_list('id', flat=True))
+        self.groups = [str(group) for group in channel_groups]
         for group in self.groups:
             await self.channel_layer.group_add(
-                str(group.id),
+                group,
                 self.channel_name
             ) 
         await self.accept()
@@ -17,7 +18,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self , close_code):
         for group in self.groups:
             await self.channel_layer.group_discard(
-                str(group.id),
+                group,
                 self.channel_name
             )
     async def receive(self, text_data):
@@ -29,25 +30,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user = await sync_to_async(User.objects.filter(first_name=username).first)()
         if action == 'join':
             # not done
-            if (group_id != group.id for group in self.groups):
-                self.groups.append(group_id)
+            if (group_id != group for group in self.groups):
+                self.groups.append(chatroom)
                 await self.channel_layer.group_add(
                     group_id,
                     self.channel_name
                 )
         elif action == 'leave':
             for group in self.groups :
-                if (group_id == str(group.id)):
+                if (group_id == group):
                     self.groups.remove(group)
                     roomUser =  await sync_to_async(ChatRoomUser.objects.filter(chatroom_id=chatroom,user_id=user).first)()
                     await self.channel_layer.group_discard(
-                        str(group_id),
+                        group,
                         self.channel_name
                     )
                     await sync_to_async(roomUser.delete)()
         elif action == 'destroy':
             for group in self.groups :
-                if (group_id == str(group.id)):
+                if (group_id == group):
                     room =  await sync_to_async(ChatRoom.objects.filter(creator=user).first)()
                     self.groups.remove(group)
                     await self.channel_layer.group_send(
@@ -61,7 +62,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                         }
                     )
                     await self.channel_layer.group_discard(
-                        str(group_id),
+                        group,
                         self.channel_name
                     )
                     await sync_to_async(room.delete)()
@@ -71,7 +72,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             new_message = Message(user=user,chatroom=chatroom,content= message)
             await sync_to_async(new_message.save)()
             database_message = await sync_to_async(Message.objects.filter(chatroom_id = chatroom, user=user ,content=message).first)()
-            if (group_id == group.id for group in self.groups):
+            if (group_id == group for group in self.groups):
                 await self.channel_layer.group_send(
                     group_id,
                     {
@@ -91,7 +92,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             database_message = await sync_to_async(Message.objects.filter(id=messageid).first)()
             database_message.content = message
             await sync_to_async(database_message.save)()
-            if (group_id == group.id for group in self.groups):
+            if (group_id == group for group in self.groups):
                 await self.channel_layer.group_send(
                     group_id,
                     {
@@ -105,8 +106,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
                             }
                     }
                 )
-            
-        elif action == 'retrieve':
+        elif action == 'retrieve_chat_details':
+            chat_room = await sync_to_async(list)(ChatRoom.objects.filter(chatroom_id=group_id).first)()
+            chat_room_users = await sync_to_async(list)(ChatRoomUser.objects.filter(chatroom_id=group_id))
+            data = []
+            for user in chat_room_users:
+                user_firstname = await sync_to_async(lambda: chat_room_users.first_name)()
+                content = message.content
+                message_id = message.id
+                data.append({
+                    'username': user_firstname,
+                    'message': content
+                })
+            await self.send(json.dumps({
+                    "type": "send_message",
+                    "action": "chat_details",
+                    "group" : group_id,
+                    "message": data
+                })
+            )
+
+        elif action == 'retrieve_chat_history':
             history = await sync_to_async(list)(Message.objects.filter(chatroom_id=group_id).order_by('timestamp'))
             data = []
 
