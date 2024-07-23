4fc2341e4e1a5d4e97c3db4f2a7142de7b7793d2
silverdead <108890290+silverdead@users.noreply.github.com>
2024-07-23 17:28:36 +0800

    Update settings.py

--- genlms/settings.py
+++ genlms/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request'
+                'lms.context_processors.user_profile',
             ],
         },
     },
@@ -175,3 +175,7 @@ MARKDOWNIFY = {
         "WHITELIST_PROTOCOLS": ['http', 'https']
    }
 }
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
