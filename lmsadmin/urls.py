56abbeb943f62897a9d6d6a48b01d17a5e1665ae
Bernard Lee <lwkbite@gmail.com>
2024-07-23 14:10:41 +0800

    Add link from Django admin portal to lmsadmin.

--- lmsadmin/urls.py
+++ lmsadmin/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='admin_index'),
     path('import_valid_rows/', views.import_valid_rows, name='import_valid_rows'),
 ]
